8bf9051d340de87aae787461eb22e7b09750c370
Peter Okafor <peter.obinna.okafor@gmail.com>
2022-03-31 09:29:22 +0100

    style: remove irrelevant styling

--- src/components/primary/ProfileImage.jsx
+++ src/components/primary/ProfileImage.jsx
@@ -3,10 +3,7 @@ import styled from "styled-components";
 const ProfileImage = styled.div`
   height: 30px;
   width: 30px;
-  left: 38px;
-  top: 36px;
   border-radius: 5px;
-  // background-color: white;
 `;
 
 export default ProfileImage
